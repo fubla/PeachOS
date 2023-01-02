eea796fb57480b9e04f0bf54224a31db84269417
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-01-02 05:46:16 -0500

    initial commit with files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Course project for Udemy's Kernel development course
+Implementation of the example OS called PeachOS by the course instructor Daniel McCarthy
---
+++ boot.asm
@@ -0,0 +1,66 @@
+ORG 0
+BITS 16
+
+_start:
+    jmp short start1
+    nop
+
+times 33 db 0
+
+start1:
+    jmp 0x7c0:start2
+
+handle_zero:
+    mov ah, 0eh
+    mov al, 'A'
+    mov bx, 0x00
+    int 0x10
+    iret
+
+handle_one:
+    mov ah, 0eh
+    mov al, 'V'
+    mov bx, 0x00
+    int 0x10
+    iret
+
+start2:
+    cli ; clear interrupts
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    xor ax, ax
+    mov ss, ax
+    mov sp, 0x7c00
+    sti ; enable interrupts
+
+    ;mov word[ss:0x00], handle_zero
+    ;mov word[ss:0x02], 0x7c0
+    mov word[ss:0x04], handle_one
+    mov word[ss:0x06], 0x7c0
+    mov ax, 0
+    div ax
+    mov si, message
+    call print
+    jmp $
+print:
+    mov bx, 0
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+.done:
+    ret    
+
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
+
+message: db 'Hello world!', 0
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
+
