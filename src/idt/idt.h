eb2dc0fbc20a04b5daccf80caaeb9fab048550bb
Nicholas Saarela <nicholas.saarela@gmail.com>
2023-01-10 21:18:17 -0500

    added and loaded IDT

---
+++ src/idt/idt.h
@@ -0,0 +1,26 @@
+#ifndef IDT_H
+#define IDT_H
+
+#include <stdint.h>
+
+
+struct idt_desc
+{
+    uint16_t offset_1; // offset bits 0-15
+    uint16_t selector; // selector in GDT
+    uint8_t zero; // unused bits
+    uint8_t type_attr; // descriptor type and attributes
+    uint16_t offset_2; // offset bits 16-31
+
+} __attribute__((packed));
+
+struct idtr_desc
+{
+    uint16_t limit; // size of desciptor table -1
+    uint32_t base; // base address of the start of the IDT
+    
+} __attribute__((packed));
+
+void idt_init();
+
+#endif
--- src/kernel.asm
+++ src/kernel.asm
@@ -1,6 +1,8 @@
 [BITS 32]
 
 global _start
+global problem
+
 extern kernel_main
 
 CODE_SEG equ 0x08
@@ -22,4 +24,8 @@ _start:
     call kernel_main
     jmp $
 
+
+problem:
+    int 0
+    
 times 512-($-$$) db 0
