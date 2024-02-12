093576972e5be0a8f47b1603b43464981b225eac
Nicholas Saarela <nicholas.saarela@gmail.com>
2024-02-12 15:16:23 -0500

    FAT32 header implemented in boot.asm filesystem write to os.bin

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -4,11 +4,30 @@ BITS 16
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
-_start:
-    jmp short start1
-    nop
+jmp short start1
+nop
 
-times 33 db 0
+OEMIdentifier       db 'PEACHOS ' ; 8 bytes space-padded
+BytesPerSector      dw 0x200      ; 512 bytes
+SectorsPerCluster   db 0x80
+ReservedSectors     dw 200
+FATCopies           db 0x02
+RootDirEntries      dw 0x40
+NumSectors          dw 0x00
+MediaType           db 0xF8
+SectorsPerFat       dw 0x100
+SectorsPerTrack     dw 0x20
+NumberOfHeads       dw 0x40
+HiddenSectors       dd 0x00
+SectorsBig          dd 0x773594
+
+DriveNumber         db 0x80
+WinNTBit            db 0x00
+Signature           db 0x29
+VolumeID            dd 0xD105
+VolumeIDString      db 'PEACHOS BOO' ; 11 bytes
+SystemIDString      db 'FAT16   ' ; 8 bytes
+
 
 start1:
     jmp 0:start2
