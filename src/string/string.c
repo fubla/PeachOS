e87b315fc74e50f90385a82b56a02de3ae1149ea
Nicholas Saarela <nicholas.saarela@gmail.com>
2024-05-21 17:14:04 -0400

    VFS fopen

--- src/string/string.c
+++ src/string/string.c
@@ -1,5 +1,15 @@
 #include "string.h"
 
+char tolower(char c)
+{
+    if (c >= 65 && c <= 90) 
+    {
+        c += 32;
+    }
+
+    return c;
+}
+
 size_t strlen(const char* str)
 {
     size_t len = 0;
@@ -16,12 +26,65 @@ size_t strnlen(const char* str, int max)
     size_t i = 0;
     for (i = 0; i < max; i++)
     {
-        if (str[i] == 0) 
+        if (str[i] == '\0') 
         {
             break;
         }
     }
     return i;
+}
+
+size_t strnlen_terminator(const char* str, int max, char terminator)
+{
+    size_t i = 0;
+    for (i = 0; i < max; i++)
+    {
+        if (str[i] == '\0' || str[i] == terminator) 
+        {
+            break;
+        }
+    }
+    return i;
+}
+
+int strncmp(const char* str1, const char* str2, int n)
+{
+    unsigned char u1, u2;
+
+    while (n-- > 0)
+    {
+        u1 = (unsigned char)*str1++;
+        u2 = (unsigned char)*str2++;
+        if (u1 != u2)
+        {
+            return u1 - u2;
+        }
+        if (u1 == '\0')
+        {
+            return 0;
+        }
+    }
+    return 0;
+}
+
+int istrncmp(const char* str1, const char* str2, int n)
+{
+    unsigned char u1, u2;
+
+    while (n-- > 0)
+    {
+        u1 = (unsigned char)*str1++;
+        u2 = (unsigned char)*str2++;
+        if (u1 != u2 && tolower(u1) != tolower(u2))
+        {
+            return u1 - u2;
+        }
+        if (u1 == '\0')
+        {
+            return 0;
+        }
+    }
+    return 0;
 }
 
 char* strcpy(char* dest, const char* src)
--- src/string/string.h
+++ src/string/string.h
@@ -4,8 +4,12 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+char tolower(char c);
 size_t strlen(const char* ptr);
 size_t strnlen(const char* str, int max);
+size_t strnlen_terminator(const char* str, int max, char terminator);
+int strncmp(const char* str1, const char* str2, int n);
+int istrncmp(const char* str1, const char* str2, int n);
 char* strcpy(char* dest, const char* src);
 bool isdigit(char c);
 int tonumericdigit(char c);
